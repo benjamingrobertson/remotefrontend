bc9a858567cee8384bbb715b5122788b5783a18a
Ben Robertson <benjamingrobertson@me.com>
2019-11-20 22:39:28 -0500

    Adds initial jest config.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   modulePathIgnorePatterns: ['<rootDir>/.cache', '<rootDir>/node_modules']
 };
+
+module.exports = {
+  ...require('./test/jest-common'),
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/**/*.stories.js'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/__tests__/',
+    '/static/',
+    '/public/'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/coverage/',
+    '/public/',
+    '/.cache/',
+    '/__mocks__/',
+    '/static/'
+  ],
+  projects: ['./test/jest.lint.js', './test/jest.client.js']
+};
