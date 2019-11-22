b487fd29e606916c62eff2154bb412dbdfc7fe3c
Ben Robertson <benjamingrobertson@me.com>
2019-11-22 09:44:53 -0500

    Add custom gatsby-plugin-performance-metrics for gtag compatibility.

---
+++ plugins/gatsby-plugin-performance-metrics/gatsby-browser.js
@@ -0,0 +1,31 @@
+import createOptions from './create-options';
+
+export const onInitialClientRender = async (_, pluginOptions) => {
+  const { firstInputDelay, useLogging, useGoogleAnalytics } = createOptions(
+    pluginOptions
+  );
+
+  console.log(pluginOptions);
+
+  if (firstInputDelay && window.perfMetrics) {
+    window.perfMetrics.onFirstInputDelay((delay, event) => {
+      if (useLogging) {
+        console.group('first-input-delay');
+        console.log('First Input Delay:', Math.round(delay));
+        console.log('Event details', event);
+        console.groupEnd();
+      }
+      if (useGoogleAnalytics && window.gtag) {
+        /* eslint-disable camelcase */
+        window.gtag('event', 'Performance Metrics', {
+          event_category: 'Performance Metrics',
+          event_action: 'first-input-delay',
+          event_label: event.type,
+          event_value: Math.round(delay),
+          nonInteraction: true
+        });
+        /* eslint-enable camelcase */
+      }
+    });
+  }
+};
